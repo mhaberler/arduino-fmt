aa577ef1e99262ff810339a5c300262f9ac5bab1
Michael Haberler <git@mah.priv.at>
2023-05-12 21:37:56 +0200

    update, examples

--- src/Fmt.h
+++ src/Fmt.h
@@ -9,14 +9,51 @@ using namespace fmt;
 class Fmt : public Stream {
 
 public:
-  Fmt(Stream &stream) : stream_(&stream){};
-  Fmt(Stream *stream) : stream_(stream){};
+  Fmt(Stream &stream, String eol = "\n") : eol_(eol), stream_(&stream){};
+  Fmt(Stream *stream, String eol = "\n") : eol_(eol), stream_(stream){};
 
   template <typename... T>
   FMT_NODISCARD FMT_INLINE int fmt(format_string<T...> fmt, T &&...args) {
     std::string s = vformat(fmt, fmt::make_format_args(args...));
     return write((const uint8_t *)s.c_str(), s.length());
   }
+
+  template <typename... T>
+  FMT_NODISCARD FMT_INLINE int fmtln(format_string<T...> fmt, T &&...args) {
+    std::string s = vformat(fmt, fmt::make_format_args(args...));
+    auto l = write((const uint8_t *)s.c_str(), s.length());
+    l += write((const uint8_t *)eol_.c_str(), eol_.length());
+    return l;
+  }
+
+  template <typename... T>
+  FMT_NODISCARD FMT_INLINE int printf(format_string<T...> fmt, T &&...args) {
+    std::string s = vformat(fmt, fmt::make_format_args(args...));
+    return write((const uint8_t *)s.c_str(), s.length());
+  }
+
+  // template <typename... T>
+  // FMT_NODISCARD FMT_INLINE int printfln(format_string<T...> fmt, T &&...args)
+  // {
+  //   // std::string s = vformat(fmt + eol_, fmt::make_format_args(args...));
+  //   // auto l = write((const uint8_t *)s.c_str(), s.length());
+  //   return 0;
+  // }
+
+  // template <typename... T>
+  // FMT_NODISCARD FMT_INLINE int print(format_string<T...> fmt, T &&...args) {
+  //   std::string s = vformat(fmt + eol_, fmt::make_format_args(args...));
+  //   auto l = write((const uint8_t *)s.c_str(), s.length());
+  //   return l;
+  // }
+
+  // template <typename... T>
+  // FMT_NODISCARD FMT_INLINE int println(format_string<T...> fmt, T &&...args)
+  // {
+  //   std::string s = vformat(fmt + eol_, fmt::make_format_args(args...));
+  //   auto l = write((const uint8_t *)s.c_str(), s.length());
+  //   return l;
+  // }
 
   size_t write(uint8_t c) { return stream_->write(c); };
 
@@ -28,5 +65,7 @@ public:
   int peek() { return 0; };
 
 private:
+  // const std::locale &locale_ = std::locale("en_US.UTF-8");
+  String eol_;
   Stream *stream_;
 };
--- src/fmttest.cpp
+++ src/fmttest.cpp
@@ -1,17 +1,44 @@
 #include <Arduino.h>
 #include <Fmt.h>
+#include <fmt/color.h>
+#include <fmt/ranges.h>
+#include <vector>
 
 Fmt f(&Serial);
 // Fmt f(Serial); // works too
+
+// fmtlib library panic handler
+extern "C" {
+void fmtlib_error(const char *file, int line, const char *message) {
+  Serial.printf("%s:%d: assertion failed: %s\n", file, line, message);
+}
+}
 
 void setup() {
   Serial.begin(115200);
   while (!Serial) {
     yield();
   }
+  // see also:
+  // https://github.com/fmtlib/fmt
+  // https://fmt.dev/latest/syntax.html#format-specification-mini-language
+
+  std::string s = fmt::format("The answer is {}.", 42);
+  s = fmt::format("The answer is {}.", 42);
+
   f.fmt("{}{}\n", "foo", 4711);
-  // all methods from Print still work:
-  f.printf("%f %s\n", 3.14, "foo");
+  f.fmtln("with implicit newline {}{}", "bar", 815);
+
+  std::vector<int> v = {1, 2, 3};
+  fmt::print("{}\n", v);
+
+  fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold, "Hello, {}!\n",
+             "world");
+  fmt::print(fg(fmt::color::floral_white) | bg(fmt::color::slate_gray) |
+                 fmt::emphasis::underline,
+             "Hello, {}!\n", "мир");
+  fmt::print(fg(fmt::color::steel_blue) | fmt::emphasis::italic, "Hello, {}!\n",
+             "世界");
 }
 
 void loop() { yield(); }
